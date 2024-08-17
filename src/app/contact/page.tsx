d951f2f070d333a00ed87ae971bdf234b3ce1455
J92 <ionutrobertbalasoiu@gmail.com>
2024-08-17 14:57:01 +0300

    textarea defaultValue

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,22 +1,27 @@
+
 import Image from "next/image";
 import styles from "./contact.module.css";
 
+
+
 const ContactPage = () => {
+
   return (
   <div className={styles.container}>
     <div className={styles.imageContainer}>
       <Image src="/images/contact.jpg" alt="contact" fill className={styles.heroImg} />
       </div>
       <div className={styles.formContainer}>
+
         <h1 className={styles.heading}>Contact Us</h1>
         <form action='' className={styles.form}>
           <input type="text" placeholder="Your Name" />
           <input type="email" placeholder="Your Email" />
           <input type="text" placeholder="Phone Number (Optional)" />
-          <textarea name="" id="">
-            Message
+          <textarea name="" id="" defaultValue='Message'>
+            
           </textarea>
-          <button>Send</button>
+          <button >Send</button>
           </form>
         </div>
   </div>
