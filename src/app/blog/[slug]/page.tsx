e2e9345ecea9265e2078f9eb5179abf7a503b987
J92 <ionutrobertbalasoiu@gmail.com>
2024-08-18 20:19:55 +0300

    vercel

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -6,7 +6,7 @@ import { getPost } from "@/app/lib/data";
 import { Metadata } from 'next';
 
 //Fetch data with an API
-const apiUrl = process.env.URL;
+const apiUrl = process.env.URL || 'http://localhost:3000';
 const getData = async (slug : any) => {
   const res = await fetch(`${apiUrl}/api/blog/${slug}`)
   
